255209890c8afd5febcd72610d38c2e125bca27a
Esther-Naserian <naseriansamuria@gmail.com>
2024-03-31 11:01:25 +0300

    add stack

---
+++ stacks.py
@@ -0,0 +1,15 @@
+
+#create a stack and add items
+students =[]
+students.append('Naserian')
+students.append('Gitahi')
+students.append('Ivy')
+students.append('Kantai')
+print(students)
+#accessing stacks
+students=['Naserian','Gitahi','Ivy','Kantai']
+print(students[1])
+#remove 
+students=['Naserian','Gitahi','Ivy','kantai']
+students.pop()
+print(students)
